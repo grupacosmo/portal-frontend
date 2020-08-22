d8aee815497b8ff8387c6cefef0ad148bef7d4d3
blaze <blazejos1230@gmail.com>
2020-08-22 09:36:37 +0200

    Removed TeamTitle component and fixed font size

--- src/views/Team/Team.js
+++ src/views/Team/Team.js
@@ -1,50 +1,52 @@
 import React from 'react';
 import './style.css';
-import { Layout, Menu} from 'antd';
-import { Tabs } from 'antd';
-import TeamTitle from "../../components/TeamTitle";
+import {Layout, Menu} from 'antd';
+import {Tabs} from 'antd';
 import MembersList from "../../components/MembersList";
 
 
-const { Header, Content, Footer } = Layout;
-const { TabPane } = Tabs;
+const {Header, Content, Footer} = Layout;
+const {TabPane} = Tabs;
 
 
 const Team = () => {
-  return (
-    <Layout>
-      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
-      </Header>
+    return (
+        <Layout>
+            <Header style={{position: 'fixed', zIndex: 1, width: '100%'}}>
+                <div className="logo"/>
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                    <Menu.Item key="1">nav 1</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+            </Header>
 
-      <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
-        <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
-            <TeamTitle title="WEBDEV" />
+            <Content className="site-layout" style={{padding: '0 50px', marginTop: 64}}>
+                <div className="site-layout-background" style={{padding: 24, minHeight: 380}}>
 
-            <Tabs defaultActiveKey="1">
-                <TabPane tab="Aktualności" key="1">
-                    Content of Tab Pane 1
-                </TabPane>
-                <TabPane tab="Członkowie" key="2">
-                    <MembersList />
-                </TabPane>
-                <TabPane tab="Materiały" key="3">
-                    Content of Tab Pane 3
-                </TabPane>
-            </Tabs>
+                    <div className="team-Title">
+                        <h1 style={{fontWeight: "bold"}}>Webdev</h1>
+                    </div>
+
+                    <Tabs defaultActiveKey="1">
+                        <TabPane tab="Aktualności" key="1">
+                            Content of Tab Pane 1
+                        </TabPane>
+                        <TabPane tab="Członkowie" key="2">
+                            <MembersList/>
+                        </TabPane>
+                        <TabPane tab="Materiały" key="3">
+                            Content of Tab Pane 3
+                        </TabPane>
+                    </Tabs>
 
 
-        </div>
-      </Content>
+                </div>
+            </Content>
 
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-    </Layout>
-  )
+            <Footer style={{textAlign: 'center'}}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+    )
 };
 
 export default Team;
