a0babb6402c560c9173e7c7b5ec42f6fdac7e7fd
Kuba Wojdyla <kwojdyla00@gmail.com>
2020-09-10 19:58:26 +0200

    Created my-profile

--- src/views/MyProfile/MyProfile.js
+++ src/views/MyProfile/MyProfile.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './style.css';
-import {Layout, Space, Row, Col, Menu} from 'antd';
+import 'antd/dist/antd.css';
+import {Layout, Space, Row, Col, Menu, Avatar, Tabs, Button} from 'antd';
 import SiderList from "../../components/SiderList";
-import {TeamOutlined,FileTextOutlined} from '@ant-design/icons';
-
-const {Header, Content, Footer} = Layout;
+import {TeamOutlined, FileTextOutlined, UserOutlined, SnippetsOutlined, BellOutlined, LogoutOutlined} from '@ant-design/icons';
 
 const projectsListData = [
     'Racing car sprays burning fuel into crowd.',
@@ -20,6 +19,18 @@ const teamsListData = [
     'Hardware',
 ];
 
+const {Header, Content, Footer} = Layout;
+const {TabPane} = Tabs;
+
+const tabBarContent = [
+    <span className="TabBarPane"><SnippetsOutlined/>Użytkownik</span>,
+    <span className="TabBarPane"><BellOutlined />Aktualności</span>,
+    <span className="TabBarPane"><TeamOutlined/>Zespoły</span>,
+    <span className="TabBarPane"><FileTextOutlined/>Projekty</span>,
+]
+
+const operations = <Button><LogoutOutlined />Wyloguj się</Button>;
+
 const MyProfile = () => {
     return (
         <Layout>
@@ -34,17 +45,41 @@ const MyProfile = () => {
 
             <Content className="site-layout" style={{padding: '0 50px', marginTop: 64}}>
                 <div className="site-layout-background" style={{padding: 24, minHeight: 380}}>
-                    <h1>My Profile</h1>
-                    <Row>
-                        <Col xs={24} md={8} lg={6}>
-                            <div>
-                                <Space size="middle" direction="vertical" style={{display:"inline"}}>
-                                    <SiderList data={projectsListData} header="Moje projekty" icon={<FileTextOutlined style={{marginRight: 8}}/>}/>
-                                    <SiderList data={teamsListData} header="Moje zespoły" icon={<TeamOutlined style={{marginRight: 8}}/>}/>
-                                </Space>
-                            </div>
-                        </Col>
-                    </Row>
+                    <h1>Mój profil</h1>
+                   
+                     <Tabs defaultActiveKey="1" tabBarExtraContent={operations}>
+                        <TabPane tab={tabBarContent[0]} key="1">
+                            <Avatar size={150} icon={<UserOutlined />}/>
+                        </TabPane>
+
+                        <TabPane tab={tabBarContent[1]} key="2">
+
+                        </TabPane>
+
+                        <TabPane tab={tabBarContent[2]} key="3">
+                            <Row>
+                                <Col xs={24} md={8} lg={6}>
+                                    <div>
+                                        <Space size="middle" direction="vertical" style={{display:"inline"}}>
+                                            <SiderList data={teamsListData} header="Moje zespoły" icon={<TeamOutlined style={{marginRight: 8}}/>}/>
+                                        </Space>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </TabPane>
+
+                        <TabPane tab={tabBarContent[3]} key="4">
+                            <Row>
+                                <Col xs={24} md={8} lg={6}>
+                                    <div>
+                                        <Space size="middle" direction="vertical" style={{display:"inline"}}>
+                                            <SiderList data={projectsListData} header="Moje projekty" icon={<FileTextOutlined style={{marginRight: 8}}/>}/>
+                                        </Space>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </TabPane>
+                    </Tabs>
                 </div>
             </Content>
 
